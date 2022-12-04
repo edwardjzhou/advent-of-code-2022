36d3cb744b225d330d9ff9c8e1619cbb54b9187d
edward <edward@utexas.edu>
2022-12-04 03:24:27 -0800

    asdf

--- day3/index.js
+++ day3/index.js
@@ -1,3 +1,41 @@
+const fs = require("fs");
+const readline = require("readline");
+const path = require("path");
+const { callbackify } = require("util");
+
+// A = 65 => 27
+// a = 97 => 1
+// function calc(char){
+//     const current = char.charCodeAt(0);
+//     if (current >= 97) {
+//         return current - 96
+//     } else {
+//         return current - 38
+//     }
+// }
+
+// async function main() {
+//   const readInterface = readline.createInterface({
+//     input: fs.createReadStream(path.join(__dirname, "data")),
+//   });
+
+//   let score = 0;
+//   for await (const line of readInterface) {
+//     const [his, mine] = [line.slice(0, line.length/2), line.slice(line.length/2)];
+//     const set = new Set()
+//     for (const char of his) {
+//         set.add(char)
+//     }
+//     for (const char of mine){
+//         if (set.has(char)) {
+//             score += calc(char)
+//             break
+//         }
+//     }
+//   }
+//   console.log(score); // 8493
+// }
+
 async function main() {
   const readInterface = readline.createInterface({
     input: fs.createReadStream(path.join(__dirname, "data")),
@@ -5,8 +43,21 @@ async function main() {
 
   let score = 0;
   for await (const line of readInterface) {
-    const [his, mine] = line.split(` `);
-
+    const [his, mine] = [
+      line.slice(0, line.length / 2),
+      line.slice(line.length / 2),
+    ];
+    const set = new Set();
+    for (const char of his) {
+      set.add(char);
+    }
+    for (const char of mine) {
+      if (set.has(char)) {
+        score += calc(char);
+        break;
+      }
+    }
   }
-  console.log(score); // 573
+  console.log(score); // 8493
 }
+main();
