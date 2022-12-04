160c45cb257ae43bf88118f2d4d379e73360298a
edward <edward@utexas.edu>
2022-12-04 03:51:31 -0800

    finish day1-4

--- day3/index.js
+++ day3/index.js
@@ -1,18 +1,17 @@
 const fs = require("fs");
 const readline = require("readline");
 const path = require("path");
-const { callbackify } = require("util");
 
 // A = 65 => 27
 // a = 97 => 1
-// function calc(char){
-//     const current = char.charCodeAt(0);
-//     if (current >= 97) {
-//         return current - 96
-//     } else {
-//         return current - 38
-//     }
-// }
+function calc(char){
+    const current = char.charCodeAt(0);
+    if (current >= 97) {
+        return current - 96
+    } else {
+        return current - 38
+    }
+}
 
 // async function main() {
 //   const readInterface = readline.createInterface({
@@ -41,23 +40,32 @@ async function main() {
     input: fs.createReadStream(path.join(__dirname, "data")),
   });
 
-  let score = 0;
+  let score = 0, count = 0;
+  let his, hers, mine
   for await (const line of readInterface) {
-    const [his, mine] = [
-      line.slice(0, line.length / 2),
-      line.slice(line.length / 2),
-    ];
-    const set = new Set();
-    for (const char of his) {
-      set.add(char);
-    }
-    for (const char of mine) {
-      if (set.has(char)) {
-        score += calc(char);
-        break;
-      }
+    if (++count % 3 === 1) {
+        his = line
+    } else if (count % 3 === 2) {
+        hers = line
+    } else if (count % 3 === 0) {
+        mine = line;
+        const hisset = new Set()
+        for (const char of his) {
+            hisset.add(char)
+        }
+        const herset = new Set() 
+        for (const char of hers) {
+            herset.add(char)
+        }
+        for (const char of mine){
+            if (hisset.has(char) && herset.has(char)) {
+                score += calc(char)
+                break
+            }
+        }
     }
   }
-  console.log(score); // 8493
+  console.log(score); // 2552
 }
+
 main();
